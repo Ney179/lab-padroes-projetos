c7bfaf5eadf94125eda0954c623d6fae4700e26d
Ney179 <84090794+Ney179@users.noreply.github.com>
2022-01-24 13:49:23 -0300

    Add files via upload
    Adicionando o projeto no repositorio

---
+++ lab-padroes-projeto-spring/src/main/java/one/digitalinnovation/gof/service/impl/ClienteServiceImpl.java
@@ -0,0 +1,61 @@
+package one.digitalinnovation.gof.service.impl;
+
+import one.digitalinnovation.gof.model.Cliente;
+import one.digitalinnovation.gof.model.Endereco;
+import one.digitalinnovation.gof.model.IClienteRepository;
+import one.digitalinnovation.gof.model.IEnderecoRepository;
+import one.digitalinnovation.gof.service.IClienteService;
+import one.digitalinnovation.gof.service.IViaCepService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+public class ClienteServiceImpl implements IClienteService {
+
+    @Autowired
+    private IClienteRepository clienteRepository;
+    @Autowired
+    private IEnderecoRepository enderecoRepository;
+    @Autowired
+    private IViaCepService viaCepService;
+
+    @Override
+    public Iterable<Cliente> buscarTodos() {
+        return clienteRepository.findAll();
+    }
+
+    @Override
+    public Cliente buscarPorId(Long id) {
+        Optional<Cliente> cliente = clienteRepository.findById(id);
+        return cliente.get();
+    }
+
+    @Override
+    public void inserir(Cliente cliente) {
+        salvarClienteComCep(cliente);
+    }
+
+    @Override
+    public void atualizar(Long id, Cliente cliente) {
+        Optional<Cliente> clienteBd = clienteRepository.findById(id);
+        if (clienteBd.isPresent()){
+            salvarClienteComCep(cliente);
+        }
+    }
+
+    @Override
+    public void deletar(Long id) {
+        clienteRepository.deleteById(id);
+    }
+
+    private void salvarClienteComCep(Cliente cliente) {
+        String cep = cliente.getEndereco().getCep();
+        Endereco endereco = enderecoRepository.findById(cep).orElseGet(() -> {
+            Endereco novoEndereco = viaCepService.consultarCep(cep);
+            enderecoRepository.save(novoEndereco);
+            return novoEndereco;
+        });
+        cliente.setEndereco(endereco);
+        clienteRepository.save(cliente);
+    }
+}
